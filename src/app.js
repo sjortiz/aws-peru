ff67172ad7951fc685c63971aadd3815a498f849
shailyn <sortizjim@gmail.com>
2020-08-08 14:11:49 -0400

    Fetching data

--- src/app.js
+++ src/app.js
@@ -2,8 +2,17 @@ import Amplify, { API, graphqlOperation } from "@aws-amplify/api";
 
 import awsconfig from "./aws-exports";
 import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
 
 Amplify.configure(awsconfig);
+
+async function getData() {
+    API.graphql(graphqlOperation(listTodos)).then((evt) => {
+        evt.data.listTodos.items.map((todo, i) => {
+            console.log(`<p>${todo.name} - ${todo.description}</p>`);
+        });
+    });
+}
 
 async function createNewTodo() {
   const todo = {
@@ -13,6 +22,7 @@ async function createNewTodo() {
 
   return await API.graphql(graphqlOperation(createTodo, { input: todo }));
 }
+
 
 MutationButton.addEventListener("click", (evt) => {
   createNewTodo().then((evt) => {
